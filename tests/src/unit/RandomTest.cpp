d2239dc75207deeb29df033ae3052245cef2635f
Gustavo Ribeiro Bodi <gustavorbodi@gmail.com>
2023-05-22 15:14:14 -0300

    Created Class for random tests

---
+++ tests/src/unit/RandomTest.cpp
@@ -0,0 +1,65 @@
+#include "libcryptosec/Random.h"
+#include <openssl/rsa.h>
+
+#include <openssl/evp.h>
+#include <openssl/ossl_typ.h>
+#include <sstream>
+#include <gtest/gtest.h>
+
+class RandomTest: public ::testing::Test {
+  protected:
+    virtual void SetUp() {
+
+    }
+
+    virtual void TearDown() {
+
+    }
+
+    void Bytes() {
+
+    }
+
+    void PseudoBytes() {
+
+    }
+
+    void SeedData() {
+
+    }
+
+    void SeedFile() {
+
+    }
+
+    void CleanSeed() {
+
+    }
+
+    void Status() {
+
+    }
+
+    static std::string bytes_test;
+    static std::string seed;
+};
+
+
+TEST_F(RandomTest, BytesTest) {
+}
+
+TEST_F(RandomTest, PseudoBytesTest) {
+}
+
+TEST_F(RandomTest, SeedDataTest) {
+}
+
+TEST_F(RandomTest, SeedFileTest) {
+}
+
+TEST_F(RandomTest, CleanSeedTest) {
+}
+
+TEST_F(RandomTest, StatusTest) {
+}
+
